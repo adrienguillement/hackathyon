9892592c421d84a78b4a9a2a9d53e9f3dff4b2cf
adrienguillement <adrien.guillement@gmail.com>
2016-10-08 12:12:37 +0200

    maj

--- pages/index.php
+++ pages/index.php
@@ -5,7 +5,7 @@ use Kernel\Connection;
 include("../include/header.php");
 
 $connect=new Connection();
-
+echo "oui";
 $tablQ=$connect->request('select * from correspondance_PDL where numCategorie=14341016');
 
 var_dump($tablQ);
