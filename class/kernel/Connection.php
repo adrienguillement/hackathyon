8a122655a42384b76e86b1ec032d49a265f935ab
adrienguillement <adrien.guillement@gmail.com>
2016-10-08 12:09:24 +0200

    oui

---
+++ class/kernel/Connection.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Kernel;
+
+class Connection
+{
+    public $_pdo;
+    const HOST='149.202.160.203';
+    const DB_NAME='enedis';
+    const USER='enedis';
+    const PW='pwsio';
+    const PORT='80';
+
+    public function __construct()
+    {
+        $this->_pdo=new \PDO('mysql:host='.self::HOST.';port='.self::PORT.';dbname='.self::DB_NAME, self::USER, self::PW);
+    }
+
+    /**
+     *
+     * @param $request
+     * @return array
+     */
+    public function request($request)
+    {
+        $q = $this->_pdo->query($request);
+        return $q->fetchAll(\PDO::FETCH_NUM);
+    }
+
+    private function getQuery($table)
+    {
+        return $this->_pdo->query('select * from '.$table);
+    }
+
+    public function getAll($table)
+    {
+        return $this->getQuery($table)->fetchAll(\PDO::FETCH_NUM);
+    }
+
+    public function get($table)
+    {
+        return $this->getQuery($table)->fetch(\PDO::FETCH_NUM);
+    }
+
+}
---
+++ pages/index.php
@@ -0,0 +1,12 @@
+<?php
+include '../class/kernel/Connection.php';
+use Kernel\Connection;
+
+include("../include/header.php");
+
+$connect=new Connection();
+
+$tablQ=$connect->request('select * from correspondance_PDL where numCategorie=14341016');
+
+var_dump($tablQ);
+
