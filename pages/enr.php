ac8c0975abd9b6128ab865a3519ac55a14e781f4
Alexis Gauvrit <alexis.gauvrit.85@gmail.com>
2016-10-09 21:46:03 +0200

    MAJ

--- pages/enr.php
+++ pages/enr.php
@@ -26,6 +26,36 @@ $html .= '</table>';
 
 echo $html;
 
+//Calcul des couts
+if(isset($_POST["surfacePP"])==null){
+    $html='<form action="./enr.php" method="POST">
+    <label>Sélectionner la surface de panneaux photovoltaîque :</label>
+    <input type="text" name="surfacePP" /></br>
+    <label>Sélectionner le nombres d\'éoliennes :</label>
+    <input type="text" name="nbEolienne" /></br>
+    <button type="submit">GO!</button>
+    </form>';
+    echo $html;
+}
+else{
+    $metre=$_POST["surfacePP"];
+    $eolienne=$_POST["nbEolienne"];
+    $coutPanneau=1000*$metre;
+    $coutEolienne=10000*$eolienne;
+    $html = '<table class="table table-striped table-hover ">';
+    $html .= '<thead>';
+    $html .= '<th>Cout panneaux photovoltaîque</th><th>Cout éoliennes</th>';
+    $html .= '</thead>';
+    $html .= '<tbody>';
+    $html .= '<tr>';
+    $html .= '<td>' . $coutPanneau . '</td>';
+    $html .= '<td>' . $coutEolienne . '</td>';
+    $html .= '</tr>';
+    $html .= '</tbody>';
+    $html .= '</table>';
+    echo $html;
+}
+
 //calcul de consommation pour chaque batiment en kW
 
 $connect = new Connection();
