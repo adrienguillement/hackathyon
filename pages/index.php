68ae7b0a7ddf067ad6def53d6eec924cca54dccd
Théo Drappier <theo.drappier85@gmail.com>
2016-10-09 18:16:14 +0200

    Req 1

--- pages/index.php
+++ pages/index.php
@@ -6,7 +6,13 @@ include("../include/header.php");
 
 $connect=new Connection();
 
-$tablQ=$connect->request('select donnees_LRY_enedis.14300186, Date from donnees_LRY_enedis');
-
-var_dump($tablQ);
-
+$tablQ=$connect->request('select numCategorie from correspondance_PDL');
+$bat=[];
+$date='01/01/2016';
+foreach($tablQ as $categ) {
+    $bat[]=$categ[0];
+    $bat[]=$date;
+    $test=$connect->request('SELECT donnees_LRY_enedis.'.$categ[0].' FROM donnees_LRY_enedis WHERE Date LIKE "'.$date.'%" ');
+    $bat[]=$test;
+}
+var_dump($bat);
