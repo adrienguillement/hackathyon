ab8ce934cdb5d932bf90018c9bad680746b007a4
adrienguillement <adrien.guillement@gmail.com>
2016-10-11 22:28:27 +0200

    22h15

--- pages/import.php
+++ pages/import.php
@@ -1,10 +1,16 @@
 <?php
 include '../class/kernel/Connection.php';
+include '../include/function.php';
 use Kernel\Connection;
 $connect=new Connection();
-$date = '30/12/2015';
-while(){
-    $tablQ=$connect->request('SELECT AVG(donnees_LRY_enedis.14303937) FROM donnes_LRY_enedis WHERE `Date=`"'.$date.'%"');
+$date = '01/12/2015';
+$tablQ=[];
+while($date!='30/09/2016'){
+    $tablQ+=[];
+    $tablQ=$connect->request('SELECT AVG(donnees_LRY_enedis.14303937) FROM donnees_LRY_enedis WHERE `Date` LIKE "'.$date.'%"');
+
+    $date = addDate($date);
 }
+var_dump($tablQ);
 
 include("../include/footer.php");
